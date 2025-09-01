fcae78c4fb40aa6ac1b3cd3b5fc07097dcd87b3e
Jamie1729 <jamieredman2@gmail.com>
2025-09-01 23:47:45 +0100

    Seperated shadcn and my custom components

--- src/components/custom/search.jsx
+++ src/components/custom/search.jsx
@@ -1,5 +1,5 @@
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
+import { Button } from "@/components/shadcn/button"
+import { Input } from "@/components/shadcn/input"
 export function Search({placeholder}) {
     return (
         <div className="flex items-center gap-2">
