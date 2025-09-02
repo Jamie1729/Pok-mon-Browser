a2d1b041dfdc021014880d8718b84a0b934d5589
Jamie1729 <jamieredman2@gmail.com>
2025-09-02 03:49:04 +0100

    Added 'type' badges for each card

--- src/components/custom/PetCard.jsx
+++ src/components/custom/PetCard.jsx
@@ -8,6 +8,7 @@ import {
     CardTitle,
 } from "@/components/shadcn/card"
 import Image from "next/image"
+import { Badge } from "@/components/shadcn/badge"
 
 export async function PetCard({id}) {
     const res =await fetch('https://pokeapi.co/api/v2/pokemon/' + id)
@@ -15,24 +16,31 @@ export async function PetCard({id}) {
 
     const src = data['sprites']['front_default']
     const name = data['name']
-    const tags = data['types']
+    const types = data['types']
 
     return (
         <div className="w-1/4 h-full p-5">
-            <Card>
-                <div className="bg-gray-200" id="img">
+            <Card id={"card-"+id}>
+                <div className="bg-gray-100" id="img">
                     <Image src={src} width={600} height={600} alt={"Picture of " + name}></Image>
                 </div>
-
-                <div id="details">
-                    <div id="id">
-                        <h1>{name}</h1>
-                        <p>#{id}</p>
-                    </div>
-                    <div id="tags">
+                <div className="pl-2 pr-2 pb-2">
+                    <div id="details">
+                        <div id="id">
+                            <h1>{name}</h1>
+                            <p>#{id}</p>
+                        </div>
+                        <div id="types">
+                            {renderTypes(types)}
+                        </div>
                     </div>
                 </div>
             </Card>
         </div>
     )
+}
+const renderTypes = (types) => {
+    return types.map((type) => {
+        return <Badge className="ml-1 mr-1" key={type.type.name}>{type.type.name}</Badge>
+    })
 }
