70ce3f44c3439a48a8f09600d572b76bf75937fd
Jamie1729 <jamieredman2@gmail.com>
2025-09-02 23:04:30 +0100

    Added string utility functions

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,3 +4,13 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
+
+export function padId(id, digits){
+  let newId = ''
+  for (let i = 0; i < digits; i++){newId += '0'}
+  return (newId + id).slice(-digits)
+}
+
+export function capFirst(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
