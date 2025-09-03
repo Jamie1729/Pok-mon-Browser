ef18206cf221e720d135af17fa4c259f64bb69f7
Jamie1729 <jamieredman2@gmail.com>
2025-09-03 04:03:39 +0100

    On detail-page: Finished StatBar card, started types card & abilities card

--- src/app/detail-page/page.js
+++ src/app/detail-page/page.js
@@ -1,20 +1,112 @@
 'use client'
 import "../globals.css"
-import {useSearchParams} from "next/navigation";
+import {padId, capFirst, cleanText, calculateWeaknesses} from "@/lib/utils"
+
+import { useEffect, useState } from "react";
+import { useSearchParams }     from "next/navigation";
+import Image                   from "next/image";
+import { Skeleton }            from "@/components/shadcn/skeleton";
+import { Card }                from "@/components/shadcn/card";
+import { StatBar }             from "@/components/custom/StatBar";
+import {renderTypes}           from "@/components/custom/PetCard";
+
 
 export default function Page() {
     const searchParams = useSearchParams()
     const id = searchParams.get('id')
-    return(
-        <div>
+    const [petData, setPetData] = useState(null)
+    const [petText, setPetText] = useState(null)
+    const [loading, setLoading] = useState(true)
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const pet_res = await fetch("https://pokeapi.co/api/v2/pokemon/" + id);
+                const pet_json = await pet_res.json();
+                setPetData(pet_json);
+                const species_res = await fetch("https://pokeapi.co/api/v2/pokemon-species/" + id);
+                const species_json = await species_res.json();
+                setPetText(cleanText(species_json['flavor_text_entries'][0]['flavor_text']));
+            } catch (err) {
+                console.error("Error fetching data:", err);
+                setPetData(null);
+                setPetData(null)
+            } finally {
+                setLoading(false);
+            }
+        };
+        if (id !== null) fetchData();
+    }, [id]);
+    return (
+        <div className="w-full">
             <header>
-                <h1 className="text-1xl font-semibold content-center pl-10">Pokémon Browser</h1>
-                <div className="w-full h-[110] bg-gray-200"></div>
+                <h1 className="text-1l font-semibold content-center pl-10 pt-4 pb-4">Pokémon Browser</h1>
+                <div className="w-full h-[110] bg-[#d1d1d1]"></div>
             </header>
-            <main id="details">
-                <h1></h1>
-            </main>
+            {loading || !petData ? (<Skeleton></Skeleton>) :
+                (<div className="w-full h-auto flex flex-col justify-center items-center pb-10">
+
+                    <div className="flex justify-center w-full">
+                        <Image src={petData['sprites']['front_default']} width={600} height={600}
+                               className="flex-row bg-[#f4f4f5] rounded-full border-[#f6f6f6] border-3 size-40 mt-[-30]"
+                               alt={"Image of " + petData['name']}/>
+                    </div>
+
+                    <div className={"flex justify-center content-center mt-1 "}>
+                        <h1 className="text-2xl font-semibold pr-1">{capFirst(petData['name'])}</h1>
+                        <p className="text-2xl text-[#71717a] font-semibold pl-1">#{padId(petData['id'], 4)}</p>
+                    </div>
+
+                    <div className="relative w-4/5 flex float-left items-center h-full rounded-xl bg-[#f5f4f4] pl-10 pt-5 pb-5 mt-5">
+                        {/* White Circle with the red-ball image */}
+                        <div
+                            className="flex shrink-0 justify-center items-center bg-white rounded-full size-16 shadow-lg">
+                            <Image src="/red-ball.png" width={223} height={226} alt="Red Pokéball"
+                                   className="object-contain p-2"/>
+                        </div>
+                        <p className="pl-4">{petText}</p>
+                    </div>
+
+                    <div className=" flex gap-4 mt-5 w-4/5 h-150 justify-center p-5 ">
+                        <Card id="biology" className="w-1/3">
+
+                        </Card>
+                        <div className="grid grid-rows-2 w-2/3">
+                            <div className="flex gap-4">
+                                <Card id="types" className="w-1/2">
+                                    <div id="types_top" className="block">
+                                        <h1 className="text-1xl font-semibold">Types</h1>
+                                        {renderTypes(petData['types'])}
+                                    </div>
+                                    <div id="types_bot">
+                                        <h1 className="text-1xl font-semibold">Weaknesses</h1>
+                                        {calculateWeaknesses(petData['types'])}
+                                    </div>
+                                </Card>
+
+                                <Card id="abilities" className="w-1/2">
+                                    <h1 className="text-1xl font-semibold">Ability</h1>
+                                </Card>
+                            </div>
+                            <Card id="stats" className="flex w-full mt-4 pt-2 pb-2">
+                                <div className="h-full">{renderStatBars(petData['stats'])}</div>
+                            </Card>
+                        </div>
+                    </div>
+                </div>)
+
+            }
         </div>
     )
+}
+function renderStatBars(stats){
+    return stats.map((stat) => {
+        return <StatBar key={stat['stat']['name']} name={stat['stat']['name']} value={stat['base_stat']}></StatBar>
+    })
+}
+function renderAbilities(abilities){
+
+}
+function renderBioInfo(){
 
 }
--- src/components/custom/StatBar.jsx
+++ src/components/custom/StatBar.jsx
@@ -2,9 +2,9 @@ import {Progress} from "@/components/shadcn/progress"
 
 export function StatBar({name, value}){
     return (
-        <div className="flex-row p-3">
-            <p className="text-semibold float-left">{name}</p>
-            <Progress className="float-right" value={value} min={0} max={100} />
+        <div className="flex pl-4 pr-4 h-1/6 items-center justify-between">
+            <h1 className="text-sm font-bold w-2/5">{name}</h1>
+            <Progress className="w-3/5" value={value} min={0} max={100} />
         </div>
     )
 }
